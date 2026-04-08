3988ac3f5fbbbaa4bd8bb47a86cb8b3c14831746
Keoner <keoniwu@gmail.com>
2026-04-08 22:22:26 +0800

    feat: created watcher & timer thread, and managed using syncer (Dependency Injection)

---
+++ watch.py
@@ -0,0 +1,71 @@
+import threading
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import time
+
+# Watchdog Tryout
+# class Watcher(FileSystemEventHandler):
+#     def on_created(self, event):
+#         print("File created: ", event.src_path)
+#         return super().on_created(event)
+#     def on_moved(self, event):
+#         print("File moved: ", event.src_path)
+#         return super().on_moved(event)
+#     def on_deleted(self, event):
+#         print("File deleted: ", event.src_path)
+#         return super().on_deleted(event)
+#     def on_modified(self, event):
+#         print("File modified: ", event.src_path)
+#         return super().on_modified(event)
+
+class Watcher(FileSystemEventHandler):
+    def __init__(self, syncer):
+        self.last_modified = {}
+        super().__init__()
+        self.syncer = syncer
+    def on_any_event(self, event):
+        self.syncer.last_event_time = time.monotonic()
+        print(event.src_path)
+        return super().on_any_event(event)
+
+class Timer(threading.Thread):
+    def __init__(self, syncer):
+        super().__init__()
+        self.syncer = syncer
+    def run(self):
+        while self.syncer.online:
+            if self.syncer.last_event_time == None:
+                time.sleep(1)
+                continue
+            if (time.monotonic() - self.syncer.last_event_time >= 10):
+                print("schedule for sync")
+                self.syncer.last_sync_time = time.monotonic()
+                self.syncer.last_event_time = None
+            time.sleep(0.5)
+
+class Syncer():
+    def __init__(self):
+        self.last_event_time = None
+        self.last_sync_time = time.monotonic()
+        self.online = True
+
+def main():
+    syncer = Syncer()
+    timer = Timer(syncer)
+    timer.start()
+    observer = Observer()
+    handler = Watcher(syncer)
+    observer.schedule(handler, ".", recursive=False)
+    observer.start()
+    
+    while True:
+        cmd = input("q to quit: ")
+        if cmd == "q":
+            syncer.online = False
+            observer.stop()
+            observer.join()
+            timer.join()
+            break
+
+if __name__ == "__main__":
+    main()
